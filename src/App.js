d03ad5627861aec2a3cfc1235f1ba4b21c0c4951
Asif <asifkhan0410@gmail.com>
2021-03-27 14:35:39 +0530

    header and search bar done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React, {useEffect, useState } from 'react'
 import './App.css';
 import searchicon from './images/search.svg'
+import openicon from './images/open.svg'
+import closeicon from './images/close.svg'
 
 function App() {  
   const [darkmode, setDarkMode] = useState(false);
   const [countryname, setCountryName] = useState('');
   const [filters, setFilters] = useState('');
+  const [togglefilter, setToggleFilter] = useState(true);
+
   return (
     <div className="App">
       <header>
@@ -13,17 +17,28 @@ function App() {
         <h3 onClick={()=> setDarkMode(!darkmode)}>{darkmode? 'Dark Mode' : 'Light Mode'}</h3>
       </header>
       <div className='search__filter'>
-        <div>
+        <div className="search">
           <img src={searchicon} alt="searchicon"/>
           <input type="search" value={countryname} onChange={(e) => setCountryName(e.target.value)} placeholder="Search for a country..."/>
         </div>
-        <select value={filters} onChange={(e)=> setFilters(e.target.value)}>
-            <option value="africa">Africa</option>
-            <option value="america">America</option>
-            <option value="asia">Asia</option>
-            <option value="europe">Europe</option>
-            <option value="oceania">Oceania</option>
-        </select>
+        <div className="filters">
+          <div className="filters__holder" onClick={() => setToggleFilter(!togglefilter)}>
+            <h3>{filters? filters : 'Filter by Region'}</h3>
+            <img src={togglefilter? openicon : closeicon} alt="filters"/>  
+          </div>
+          {!togglefilter? 
+           <div className="holder__options">
+             <option value="Africa" onClick={(e)=> {setFilters(e.target.value); setToggleFilter(!togglefilter)}}>Africa</option>
+             <option value="America" onClick={(e)=> {setFilters(e.target.value); setToggleFilter(!togglefilter)}}>America</option>
+             <option value="Asia" onClick={(e)=> {setFilters(e.target.value); setToggleFilter(!togglefilter)}}>Asia</option>
+             <option value="Europe" onClick={(e)=> {setFilters(e.target.value); setToggleFilter(!togglefilter)}}>Europe</option>
+             <option value="Oceania" onClick={(e)=> {setFilters(e.target.value); setToggleFilter(!togglefilter)}}>Oceania</option>
+           </div>
+          : ""}
+        </div>
+      </div>
+      <div className="countries">
+        
       </div>
     </div>
   );
